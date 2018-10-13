80cb87a4bcd9d4c8a2f35381e98b50c9f70bfb17
Miguel Angel Jimenez Huerta <miguel@MacBook-Pro-de-Miguel.local>
2018-10-13 11:09:21 -0500

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+use_frameworks!
+
+target 'Wundercast' do
+    project 'Wundercast'
+    pod 'RxSwift'
+    pod 'RxCocoa'
+    pod 'SwiftyJSON'
+end
---
+++ Wundercast/ViewController.swift
@@ -0,0 +1,162 @@
+/*
+ * Copyright (c) 2014-2016 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import RxSwift
+import RxCocoa
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var mapView: MKMapView!
+  @IBOutlet weak var mapButton: UIButton!
+  @IBOutlet weak var geoLocationButton: UIButton!
+  @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet weak var searchCityName: UITextField!
+  @IBOutlet weak var tempLabel: UILabel!
+  @IBOutlet weak var humidityLabel: UILabel!
+  @IBOutlet weak var iconLabel: UILabel!
+  @IBOutlet weak var cityNameLabel: UILabel!
+
+  let bag = DisposeBag()
+  let locationManager = CLLocationManager()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+
+    style()
+
+    let searchInput = searchCityName.rx.controlEvent(.editingDidEndOnExit).asObservable()
+      .map { self.searchCityName.text ?? ""}
+      .filter { ($0).count > 0 }
+    
+    let textSearch = searchInput.flatMap { (name) in
+      return ApiController.shared.currentWeather(city: name)
+      .catchErrorJustReturn(ApiController.Weather.dummy)
+    }
+    
+    let currentLocation = locationManager.rx.didUpdateLocations
+      .map({ (locations) in
+        return locations[0]
+      })
+      .filter({ (location) -> Bool in
+        return location.horizontalAccuracy < kCLLocationAccuracyHundredMeters
+      })
+      .map({$0.coordinate})
+    
+    let geoInput = geoLocationButton.rx.tap.asObservable()
+      .do(onNext: { (_) in
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+      })
+   
+    let geoLocation = geoInput.flatMap { _ in
+      return currentLocation.take(1)
+    }
+    
+    let geoSearch = geoLocation.flatMap { (location) in
+      return ApiController.shared.currentWeather(lat: location.latitude, lon: location.longitude)
+        .catchErrorJustReturn(ApiController.Weather.dummy)
+    }
+    
+    let search = Observable.from([textSearch,geoSearch])
+        .merge()
+        .asDriver(onErrorJustReturn: ApiController.Weather.dummy)
+    
+    search.map { "\($0.temperature)° C" }
+      .drive(tempLabel.rx.text)
+      .disposed(by: bag)
+    
+    search.map { $0.icon }
+      .drive(iconLabel.rx.text)
+      .disposed(by: bag)
+    
+    search.map { "\($0.humidity)%" }
+      .drive(humidityLabel.rx.text)
+      .disposed(by: bag)
+    
+    search.map { $0.cityName }
+      .drive(cityNameLabel.rx.text)
+      .disposed(by: bag)
+    
+    let running = Observable.from([
+      searchInput.map({_ in true}),
+      geoInput.map({ _ in true}),
+      search.map({_ in false}).asObservable()
+      ])
+      .merge()
+      .startWith(true)
+      .asDriver(onErrorJustReturn: false)
+    
+    running
+      .skip(1)
+      .drive(activityIndicator.rx.isAnimating)
+      .disposed(by: bag)
+    
+    running
+      .drive(tempLabel.rx.isHidden)
+      .disposed(by: bag)
+    running
+      .drive(iconLabel.rx.isHidden)
+      .disposed(by: bag)
+    running
+      .drive(humidityLabel.rx.isHidden)
+      .disposed(by: bag)
+    running
+      .drive(cityNameLabel.rx.isHidden)
+      .disposed(by: bag)
+    
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+  }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+
+    Appearance.applyBottomLine(to: searchCityName)
+  }
+
+  override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  // MARK: - Style
+
+  private func style() {
+    view.backgroundColor = UIColor.aztec
+    searchCityName.textColor = UIColor.ufoGreen
+    tempLabel.textColor = UIColor.cream
+    humidityLabel.textColor = UIColor.cream
+    iconLabel.textColor = UIColor.cream
+    cityNameLabel.textColor = UIColor.cream
+  }
+}
+
